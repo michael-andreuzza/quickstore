aabab7faf04e7151717e3f87fad7a4c370537fd1
Nikk Nguyen <ndp190@gmail.com>
2023-11-19 13:42:22 +0700

    Add vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: 'https://lexingtonthemes.com',
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
