e418cbf20793ec64714f56a47b14f7a7853c559e
Oscar Byrne <osbyrne1@gmail.com>
2024-09-20 13:26:29 +0200

    wow
    Co-authored-by: Lowis <0x4C6F7769730A@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fonts: {
-        sans: ['Open Sans', 'system-ui'],
-        serif: ['Merriweather', 'Georgia'],
-        monospace: ['Monaco', 'Consolas'],
+    extend: {
+      fontFamily: {
+        EditorialNewRegular: ['EditorialNewRegular'],
+        EditorialNewLight: ['EditorialNewLight'],
+        EditorialNewUltrabold: ['EditorialNewUltrabold'],
+        EditorialNewUltralight: ['EditorialNewUltralight'],
+        EditorialNewUltrabolditalic: ['EditorialNewUltraboldItalic'],
+        EditorialNewUltralightitalic: ['EditorialNewUltralightItalic'],
+      },
     },
   },
   plugins: [],
